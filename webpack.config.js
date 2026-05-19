00bdf561214621b4ffa38d980e34abaa5423950d
dem1urgeTit <twentysevenowls@gmail.com>
2026-05-19 19:22:51 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,21 @@ module.exports = {
 
   devtool: 'source-map',
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
