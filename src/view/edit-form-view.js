d78e60dafa7de32ef465808c4dab92e1bc792fbb
dem1urgeTit <twentysevenowls@gmail.com>
2026-05-24 14:00:11 +0500

    Update edit-form-view.js

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -1,49 +1,144 @@
+
 export default class EditFormView {
-  constructor(pointData = null) {
-    this._point = pointData;
+  constructor(point = null, destination = null, selectedOffers = [], allOffersByType = []) {
+    this._point = point;
+    this._destination = destination;
+    this._selectedOffers = selectedOffers;
+    this._allOffersByType = allOffersByType;
     this._element = null;
   }
 
   getTemplate() {
-          <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-            <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
-              <label class="trip-sort__btn" for="sort-day">Day</label>
-            </div>
+    const point = this._point || {};
+    const destination = this._destination || {};
+    const selectedOffers = this._selectedOffers || [];
+    const allOffers = this._allOffersByType || [];
 
-            <div class="trip-sort__item  trip-sort__item--event">
-              <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-              <label class="trip-sort__btn" for="sort-event">Event</label>
-            </div>
+    const {
+      id = '',
+      type = 'flight',
+      startDateTime = '',
+      endDateTime = '',
+      basePrice = 0,
+      isFavorite = false,
+      destinationId = '',
+      offersIds = []
+    } = point;
 
-            <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-              <label class="trip-sort__btn" for="sort-time">Time</label>
-            </div>
+    const destinationName = destination.name || '';
+    const destinationDescription = destination.description || '';
+    const destinationPictures = destination.pictures || [];
 
-            <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-              <label class="trip-sort__btn" for="sort-price">Price</label>
-            </div>
+    const formatDateForInput = (isoString) => {
+      if (!isoString) return '';
+      return isoString.slice(0, 16);
+    };
 
-            <div class="trip-sort__item  trip-sort__item--offer">
-              <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-              <label class="trip-sort__btn" for="sort-offer">Offers</label>
-            </div>
-          </form>
+    const startDateValue = formatDateForInput(startDateTime);
+    const endDateValue = formatDateForInput(endDateTime);
+
+    const eventTypes = [
+      'taxi', 'bus', 'train', 'ship', 'drive', 'flight',
+      'check-in', 'sightseeing', 'restaurant'
+    ];
+
+    const typesRadios = eventTypes.map(t => `
+      <div class="event__type-item">
+        <input id="event-type-${t}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${t}" ${t === type ? 'checked' : ''}>
+        <label class="event__type-label  event__type-label--${t}" for="event-type-${t}-1">${t}</label>
+      </div>
+    `).join('');
+
+    const offersCheckboxes = allOffers.map(offer => {
+      const isChecked = selectedOffers.some(selected => selected.id === offer.id);
+      return `
+        <div class="event__offer-selector">
+          <input class="event__offer-checkbox  visually-hidden" id="offer-${offer.id}" type="checkbox" name="offer" value="${offer.id}" ${isChecked ? 'checked' : ''}>
+          <label class="event__offer-label" for="offer-${offer.id}">
+            <span class="event__offer-title">${offer.title}</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">${offer.price}</span>
+          </label>
+        </div>
+      `;
+    }).join('');
+
+    const picturesHtml = destinationPictures.map((pic, idx) => `
+      <img class="event__photo" src="${pic.src}" alt="${pic.description}">
+    `).join('');
+
     return `
       <form class="event event--edit" action="#" method="post">
-        <!-- Здесь будет разметка формы редактирования события -->
-        <p>markup/edit-point.html</p>
+        <header class="event__header">
+          <div class="event__type-wrapper">
+            <label class="event__type  event__type-btn" for="event-type-toggle-1">
+              <span class="visually-hidden">Choose event type</span>
+              <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+            </label>
+            <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+            <div class="event__type-list">
+              <fieldset class="event__type-group">
+                <legend class="visually-hidden">Event type</legend>
+                ${typesRadios}
+              </fieldset>
+            </div>
+          </div>
+
+          <div class="event__field-group  event__field-group--destination">
+            <label class="event__label  event__type-output" for="event-destination-1">${type}</label>
+            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationName}" list="destination-list-1">
+            <datalist id="destination-list-1">
+              <!-- Здесь можно динамически подставить список городов -->
+            </datalist>
+          </div>
+
+          <div class="event__field-group  event__field-group--time">
+            <label class="visually-hidden" for="event-start-time-1">From</label>
+            <input class="event__input  event__input--time" id="event-start-time-1" type="datetime-local" name="event-start-time" value="${startDateValue}">
+            &mdash;
+            <label class="visually-hidden" for="event-end-time-1">To</label>
+            <input class="event__input  event__input--time" id="event-end-time-1" type="datetime-local" name="event-end-time" value="${endDateValue}">
+          </div>
+
+          <div class="event__field-group  event__field-group--price">
+            <label class="visually-hidden" for="event-price-1">Price</label>
+            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+          </div>
+
+          <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+          <button class="event__reset-btn" type="reset">Delete</button>
+          <button class="event__rollup-btn" type="button">
+            <span class="visually-hidden">Close event</span>
+          </button>
+        </header>
+
+        <section class="event__details">
+          <section class="event__section  event__section--offers">
+            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+            <div class="event__available-offers">
+              ${offersCheckboxes || '<p>No offers available</p>'}
+            </div>
+          </section>
+
+          <section class="event__section  event__section--destination">
+            <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+            <p class="event__destination-description">${destinationDescription}</p>
+            <div class="event__photos-container">
+              <div class="event__photos-tape">
+                ${picturesHtml || '<p>No photos</p>'}
+              </div>
+            </div>
+          </section>
+        </section>
       </form>
     `;
   }
 
   getElement() {
     if (!this._element) {
-      this._element = document.createElement('div');
+      this._element = document.createElement('li');
+      this._element.className = 'trip-events__item';
       this._element.innerHTML = this.getTemplate();
-      this._element = this._element.firstElementChild;
     }
     return this._element;
   }
