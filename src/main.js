846ff4d270fbbd997c35c70b177456e0f2d90a9a
dem1urgeTit <twentysevenowls@gmail.com>
2026-05-24 16:22:28 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import ApiService from './src/api-service.js';
 import TripModel from './src/model/trip-model.js';
 import FilterModel from './src/model/filter-model.js';
 import TripPresenter from './src/presenter/trip-presenter.js';
 import FilterPresenter from './src/presenter/filter-presenter.js';
 
-const tripModel = new TripModel();
+const api = new ApiService('https://21.objects.pages.academy/big-trip', 'Basic yourRandomSecretKey');
+const tripModel = new TripModel(api);
 const filterModel = new FilterModel();
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
