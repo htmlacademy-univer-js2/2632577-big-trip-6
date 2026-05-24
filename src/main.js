a01115efb723da29521accdccdb0cfb43a4c3acb
dem1urgeTit <twentysevenowls@gmail.com>
2026-05-24 15:45:10 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import TripPresenter from './src/presenter/trip-presenter.js';
 import TripModel from './src/model/trip-model.js';
+import FilterModel from './src/model/filter-model.js';
+import TripPresenter from './src/presenter/trip-presenter.js';
+import FilterPresenter from './src/presenter/filter-presenter.js';
 
-const model = new TripModel();
-const presenter = new TripPresenter(model);
-presenter.init();
+const tripModel = new TripModel();
+const filterModel = new FilterModel();
+
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const filterPresenter = new FilterPresenter(filtersContainer, filterModel, tripModel);
+filterPresenter.init();
+
+const tripPresenter = new TripPresenter(tripModel, filterModel);
+tripPresenter.init();
